abf5b44b9e164528d00cf83c522603205095e7ce
Mirko Grönroos <mirko@mirko.fi>
2026-05-11 17:59:08 +0000

    alea 0.9.6

--- Formula/alea.rb
+++ Formula/alea.rb
@@ -1,20 +1,20 @@
 class Alea < Formula
   desc "Verifiable random selection using drand public randomness"
   homepage "https://github.com/hermo/alea"
-  version "0.9.5"
+  version "0.9.6"
   license "GPL-2.0-only"
 
   on_macos do
     on_arm do
-      url "https://github.com/hermo/alea/releases/download/v0.9.5/alea-aarch64-apple-darwin.tar.gz"
-      sha256 "3be27f5c1f9d6e4fb58fbc1df52fee4170f4f60ab9c1618a918ed9260d20c4df"
+      url "https://github.com/hermo/alea/releases/download/v0.9.6/alea-aarch64-apple-darwin.tar.gz"
+      sha256 "7f6d4e393ca3b6b53e7270cec57489bca6085e6dec29be2fa5b102ce86fdcf20"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/hermo/alea/releases/download/v0.9.5/alea-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9b46e6c27b83c80a2da8cd23308d71eede0b6c89007bc2e0cc73337083182422"
+      url "https://github.com/hermo/alea/releases/download/v0.9.6/alea-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "f139184ce17a9fb6f7285390030a1a71cb97e6d8b86179c5148594475e4995fe"
     end
   end
 
