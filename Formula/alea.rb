6854a6aef3d72d0deac27c1f63c669aa1a8779ec
Mirko Grönroos <mirko@mirko.fi>
2026-05-12 15:04:37 +0000

    alea 0.9.9

--- Formula/alea.rb
+++ Formula/alea.rb
@@ -1,25 +1,26 @@
 class Alea < Formula
   desc "Verifiable random selection using drand public randomness"
   homepage "https://github.com/hermo/alea"
-  version "0.9.8"
+  version "0.9.9"
   license "GPL-2.0-only"
 
   on_macos do
     on_arm do
-      url "https://github.com/hermo/alea/releases/download/v0.9.8/alea-aarch64-apple-darwin.tar.gz"
-      sha256 "0f08120d2d9cf2282613fe6f86c1c7e2335400f70b166144fdcaae9df291a64d"
+      url "https://github.com/hermo/alea/releases/download/v0.9.9/alea-aarch64-apple-darwin.tar.gz"
+      sha256 "21ff0975f18d1393707a3bcaefb6125257eea3cacc808ddc4d9e408fb2933a64"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/hermo/alea/releases/download/v0.9.8/alea-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "5c28d7401b9ca86cc15cf4dbc0f849a41964941a837d7f72ea794eee0003b8fb"
+      url "https://github.com/hermo/alea/releases/download/v0.9.9/alea-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "bf9f38e0e830e14130858fe7c58088ca467110b8f4498eca11febcc0efad8a03"
     end
   end
 
   def install
     bin.install "alea"
+    man1.install "alea.1"
   end
 
   test do
